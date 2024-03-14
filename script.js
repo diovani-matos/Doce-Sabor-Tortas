64d26f6d7be7e98cec860695fb747c1b41e00a23
Diovani <matosdiovani@gmail.com>
2024-03-14 18:56:39 -0300

    alteração na data de abertura do restaurante

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkOpen() {
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 18.5 && hora < 23;
+  return hora >= 18 && hora < 23;
 }
 
 const spanItem = document.getElementById("date-span");
