e2c7be2d13ea03e11529b82ce793af0b71847d0e
Diovani <matosdiovani@gmail.com>
2024-04-03 20:05:29 -0300

    Alteração imagem home para svg, cores dos botoes de add ao carrinho ajuste no botão de modal versão mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,8 @@ module.exports = {
       sans: ["Lato", "sans-serif"],
     },
     extend: {
-      backgroundImage: { home: "url(/assets/background3.jpg)" },
-      colors: {
-        customgreen: "#98FB98",
-        chocolate: "#8B4513",
-        custompink: "#FF6F61",
-        creme: "#FFFACD",
-      },
+      backgroundImage: { home: "url(/assets/background.svg)" },
+      
     },
   },
   plugins: [],
